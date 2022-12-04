6e00b66bf598ebda2b9d5505ada6bcd9856cd868
Mryamfzli2003 <117342363+Mryamfzli2003@users.noreply.github.com>
2022-12-04 22:20:54 +0330

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+ main() {
+ 	int n,m,i,g;
+ 	cout<<"please enter n:";
+ 	cin>>n;
+ 	cout<<"please enter m:";
+ 	cin>>m;
+ 	
+ 	for(i=1;i<=n && i<=m; i++){
+ 		if(n%i==0 && m%i==0)
+ 		g=i;
+	 }
+ 	if(n%m==0);
+ 	
+ 	cout<<"g"<<"\t"<<n<<"\t"<<"and"<<"\t"<<m<<"\t"<<"is"<<"\t"<<g;
+ 	
+
+}
